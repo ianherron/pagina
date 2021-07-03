3f7622dd2e781c2e6ef1911ff6ad39e7d61217da
ianherron <86888100+ianherron@users.noreply.github.com>
2021-07-03 10:52:12 -0600

    Add files via upload

---
+++ lmtlss.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+ScrollReveal().reveal('.showcase', { delay: 500 });
+ScrollReveal().reveal('.mission', { delay: 500 });
+ScrollReveal().reveal('.vision', { delay: 500 });
+ScrollReveal().reveal('.values', { delay: 500 });
+ScrollReveal().reveal('.footer-links', { delay: 500 });
+ScrollReveal().reveal('.footer', { delay: 500 });
+ScrollReveal().reveal('.quote', { delay: 500 });
+
