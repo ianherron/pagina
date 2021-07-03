ccbc7aed138506d2ad9bc6fc24d593eb75687591
ianherron <86888100+ianherron@users.noreply.github.com>
2021-07-03 14:03:18 -0600

    Update lmtlss.js

--- lmtlss.js
+++ lmtlss.js
@@ -7,7 +7,5 @@ ScrollReveal().reveal('.showcase', { delay: 500 });
 ScrollReveal().reveal('.mission', { delay: 500 });
 ScrollReveal().reveal('.vision', { delay: 500 });
 ScrollReveal().reveal('.values', { delay: 500 });
-ScrollReveal().reveal('.footer-links', { delay: 500 });
-ScrollReveal().reveal('.footer', { delay: 500 });
 ScrollReveal().reveal('.quote', { delay: 500 });
 
